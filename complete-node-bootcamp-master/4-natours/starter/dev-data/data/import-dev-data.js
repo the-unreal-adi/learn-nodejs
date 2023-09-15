f140f9dafe77c055f11552311098db95e3c07bc2
Aditya Chaturvedi <aditya.sc@nic.in>
2023-09-15 17:48:03 +0530

    Added indexing for fast reading in tour model Added import options for user and review model in import-dev-data.js

--- complete-node-bootcamp-master/4-natours/starter/dev-data/data/import-dev-data.js
+++ complete-node-bootcamp-master/4-natours/starter/dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -17,9 +19,16 @@ mongoose
   });
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'),
+);
+
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Import Successful');
   } catch (err) {
     console.log(err);
@@ -30,6 +39,8 @@ const importData = async () => {
 const deleteAllData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Delete Successful');
   } catch (err) {
     console.log(err);
